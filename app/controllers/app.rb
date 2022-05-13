37540a163da59c77373378ddb0342ae91df90876
Soumya Ray <soumya.ray@gmail.com>
2022-05-13 14:03:15 +0800

    Sends out verification email and requires token-based authorization
    - Handle visit from email verification link
    - Creates authorization token upon login

--- app/controllers/app.rb
+++ app/controllers/app.rb
@@ -3,17 +3,27 @@
 require 'roda'
 require 'json'
 
+require_relative 'http_request'
+
 module Credence
   # Web controller for Credence API
   class Api < Roda
     plugin :halt
     plugin :multi_route
+    plugin :request_headers
 
     route do |routing|
       response['Content-Type'] = 'application/json'
+      request = HttpRequest.new(routing)
 
-      HttpRequest.new(routing).secure? ||
+      request.secure? ||
         routing.halt(403, { message: 'TLS/SSL Required' }.to_json)
+
+      begin
+        @auth_account = request.authenticated_account
+      rescue AuthToken::InvalidTokenError
+        routing.halt 403, { message: 'Invalid auth token' }.to_json
+      end
 
       routing.root do
         { message: 'CredenceAPI up at /api/v1' }.to_json

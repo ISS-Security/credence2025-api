ISS-Security/credence2021-api

Use security policies to manage all resources

app/controllers/projects.rb
@@ -1,83 +1,119 @@
 # frozen_string_literal: true
 
-require 'roda'
 require_relative './app'
 
+# rubocop:disable Metrics/BlockLength
 module Credence
   # Web controller for Credence API
   class Api < Roda
     route('projects') do |routing|
+      unauthorized_message = { message: 'Unauthorized Request' }.to_json
+      routing.halt(403, unauthorized_message) unless @auth_account
+
       @proj_route = "#{@api_root}/projects"
-
       routing.on String do |proj_id|
-        routing.on 'documents' do
-          @doc_route = "#{@api_root}/projects/#{proj_id}/documents"
-          # GET api/v1/projects/[proj_id]/documents/[doc_id]
-          routing.get String do |doc_id|
-            doc = Document.where(project_id: proj_id, id: doc_id).first
-            doc ? doc.to_json : raise('Document not found')
-          rescue StandardError => e
-            routing.halt 404, { message: e.message }.to_json
-          end
+        @req_project = Project.first(id: proj_id)
 
-          # GET api/v1/projects/[proj_id]/documents
-          routing.get do
-            output = { data: Project.first(id: proj_id).documents }
-            JSON.pretty_generate(output)
-          rescue StandardError
-            routing.halt(404, { message: 'Could not find documents' }.to_json)
-          end
+        # GET api/v1/projects/[ID]
+        routing.get do
+          project = GetProjectQuery.call(
+            account: @auth_account, project: @req_project
+          )
 
+          { data: project }.to_json
+        rescue GetProjectQuery::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetProjectQuery::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          puts "FIND PROJECT ERROR: #{e.inspect}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
+
+        routing.on('documents') do
           # POST api/v1/projects/[proj_id]/documents
           routing.post do
-            new_data = JSON.parse(routing.body.read)
-
-            new_doc = CreateDocumentForProject.call(
-              project_id: proj_id, document_data: new_data
+            new_document = CreateDocument.call(
+              account: @auth_account,
+              project: @req_project,
+              document_data: JSON.parse(routing.body.read)
             )
 
             response.status = 201
-            response['Location'] = "#{@doc_route}/#{new_doc.id}"
-            { message: 'Document saved', data: new_doc }.to_json
-          rescue Sequel::MassAssignmentRestriction
-            routing.halt 400, { message: 'Illegal Request' }.to_json
-          rescue StandardError
-            routing.halt 500, { message: 'Database error' }.to_json
+            response['Location'] = "#{@doc_route}/#{new_document.id}"
+            { message: 'Document saved', data: new_document }.to_json
+          rescue CreateDocument::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue CreateDocument::IllegalRequestError => e
+            routing.halt 400, { message: e.message }.to_json
+          rescue StandardError => e
+            puts "CREATE_DOCUMENT_ERROR: #{e.inspect}"
+            routing.halt 500, { message: 'API server error' }.to_json
           end
         end
 
-        # GET api/v1/projects/[proj_id]
-        routing.get do
-          proj = Project.first(id: proj_id)
-          proj ? proj.to_json : raise('Project not found')
-        rescue StandardError => e
-          routing.halt 404, { message: e.message }.to_json
+        routing.on('collaborators') do
+          # PUT api/v1/projects/[proj_id]/collaborators
+          routing.put do
+            req_data = JSON.parse(routing.body.read)
+
+            collaborator = AddCollaborator.call(
+              account: @auth_account,
+              project: @req_project,
+              collab_email: req_data['email']
+            )
+
+            { data: collaborator }.to_json
+          rescue AddCollaborator::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue StandardError
+            routing.halt 500, { message: 'API server error' }.to_json
+          end
+
+          # DELETE api/v1/projects/[proj_id]/collaborators
+          routing.delete do
+            req_data = JSON.parse(routing.body.read)
+            collaborator = RemoveCollaborator.call(
+              req_username: @auth_account.username,
+              collab_email: req_data['email'],
+              project_id: proj_id
+            )
+
+            { message: "#{collaborator.username} removed from projet",
+              data: collaborator }.to_json
+          rescue RemoveCollaborator::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue StandardError
+            routing.halt 500, { message: 'API server error' }.to_json
+          end
         end
       end
 
-      # GET api/v1/projects
-      routing.get do
-        account = Account.first(username: @auth_account['username'])
-        projects = account.projects
-        JSON.pretty_generate(data: projects)
-      rescue StandardError
-        routing.halt 403, { message: 'Could not find any projects' }.to_json
-      end
+      routing .is do
+        # GET api/v1/projects
+        routing.get do
+          projects = ProjectPolicy::AccountScope.new(@auth_account).viewable
 
-      # POST api/v1/projects
-      routing.post do
-        new_data = JSON.parse(routing.body.read)
-        new_proj = Project.new(new_data)
-        raise('Could not save project') unless new_proj.save
+          JSON.pretty_generate(data: projects)
+        rescue StandardError
+          routing.halt 403, { message: 'Could not find any projects' }.to_json
+        end
 
-        response.status = 201
-        response['Location'] = "#{@proj_route}/#{new_proj.id}"
-        { message: 'Project saved', data: new_proj }.to_json
-      rescue Sequel::MassAssignmentRestriction
-        routing.halt 400, { message: 'Illegal Request' }.to_json
-      rescue StandardError => e
-        routing.halt 500, { message: e.message }.to_json
+        # POST api/v1/projects
+        routing.post do
+          new_data = JSON.parse(routing.body.read)
+          new_proj = @auth_account.add_owned_project(new_data)
+
+          response.status = 201
+          response['Location'] = "#{@proj_route}/#{new_proj.id}"
+          { message: 'Project saved', data: new_proj }.to_json
+        rescue Sequel::MassAssignmentRestriction
+          routing.halt 400, { message: 'Illegal Request' }.to_json
+        rescue StandardError
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
       end
     end
   end
 end
+# rubocop:enable Metrics/BlockLength

ISS-Security/credence2025-api

app/controllers/projects.rb
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-require 'roda'
 require_relative './app'
 
 module Credence
@@ -8,80 +7,110 @@ module Credence
   class Api < Roda
     # rubocop:disable Metrics/BlockLength
     route('projects') do |routing|
+      unauthorized_message = { message: 'Unauthorized Request' }.to_json
+      routing.halt(403, unauthorized_message) unless @auth_account
+
       @proj_route = "#{@api_root}/projects"
-
       routing.on String do |proj_id|
-        routing.on 'documents' do
-          @doc_route = "#{@api_root}/projects/#{proj_id}/documents"
-          # GET api/v1/projects/[proj_id]/documents/[doc_id]
-          routing.get String do |doc_id|
-            doc = Document.where(project_id: proj_id, id: doc_id).first
-            doc ? doc.to_json : raise('Document not found')
-          rescue StandardError => e
-            routing.halt 404, { message: e.message }.to_json
-          end
+        @req_project = Project.first(id: proj_id)
 
-          # GET api/v1/projects/[proj_id]/documents
-          routing.get do
-            output = { data: Project.first(id: proj_id).documents }
-            JSON.pretty_generate(output)
-          rescue StandardError
-            routing.halt(404, { message: 'Could not find documents' }.to_json)
-          end
+        # GET api/v1/projects/[ID]
+        routing.get do
+          project = GetProjectQuery.call(
+            account: @auth_account, project: @req_project
+          )
 
+          { data: project }.to_json
+        rescue GetProjectQuery::ForbiddenError => e
+          routing.halt 403, { message: e.message }.to_json
+        rescue GetProjectQuery::NotFoundError => e
+          routing.halt 404, { message: e.message }.to_json
+        rescue StandardError => e
+          puts "FIND PROJECT ERROR: #{e.inspect}"
+          routing.halt 500, { message: 'API server error' }.to_json
+        end
+
+        routing.on('documents') do
           # POST api/v1/projects/[proj_id]/documents
           routing.post do
-            new_data = HttpRequest.new(routing).body_data
-
-            new_doc = CreateDocumentForProject.call(
-              project_id: proj_id, document_data: new_data
+            new_document = CreateDocument.call(
+              account: @auth_account,
+              project: @req_project,
+              document_data: HttpRequest.new(routing).body_data
             )
 
             response.status = 201
-            response['Location'] = "#{@doc_route}/#{new_doc.id}"
-            { message: 'Document saved', data: new_doc }.to_json
-          rescue Sequel::MassAssignmentRestriction
-            Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-            routing.halt 400, { message: 'Illegal Attributes' }.to_json
+            response['Location'] = "#{@doc_route}/#{new_document.id}"
+            { message: 'Document saved', data: new_document }.to_json
+          rescue CreateDocument::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue CreateDocument::IllegalRequestError => e
+            routing.halt 400, { message: e.message }.to_json
           rescue StandardError => e
             Api.logger.warn "DOCUMENT SAVING ERROR: #{e.message}"
             routing.halt 500, { message: 'Error creating document' }.to_json
           end
         end
 
-        # GET api/v1/projects/[proj_id]
-        routing.get do
-          proj = Project.first(id: proj_id)
-          proj ? proj.to_json : raise('Project not found')
-        rescue StandardError => e
-          routing.halt 404, { message: e.message }.to_json
+        routing.on('collaborators') do
+          # PUT api/v1/projects/[proj_id]/collaborators
+          routing.put do
+            req_data = JSON.parse(routing.body.read)
+
+            collaborator = AddCollaborator.call(
+              account: @auth_account,
+              project: @req_project,
+              collab_email: req_data['email']
+            )
+
+            { data: collaborator }.to_json
+          rescue AddCollaborator::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue StandardError
+            routing.halt 500, { message: 'API server error' }.to_json
+          end
+
+          # DELETE api/v1/projects/[proj_id]/collaborators
+          routing.delete do
+            req_data = JSON.parse(routing.body.read)
+            collaborator = RemoveCollaborator.call(
+              req_username: @auth_account.username,
+              collab_email: req_data['email'],
+              project_id: proj_id
+            )
+
+            { message: "#{collaborator.username} removed from projet",
+              data: collaborator }.to_json
+          rescue RemoveCollaborator::ForbiddenError => e
+            routing.halt 403, { message: e.message }.to_json
+          rescue StandardError
+            routing.halt 500, { message: 'API server error' }.to_json
+          end
         end
       end
 
-      # GET api/v1/projects/
-      routing.get do
-        account = Account.first(username: @auth_account['username'])
-        projects = account.projects
-        JSON.pretty_generate(data: projects)
-      rescue StandardError
-        routing.halt 403, { message: 'Could not find any projects' }.to_json
-      end
+      routing.is do
+        # GET api/v1/projects
+        routing.get do
+          projects = ProjectPolicy::AccountScope.new(@auth_account).viewable
 
-      # POST api/v1/projects
-      routing.post do
-        new_data = HttpRequest.new(routing).body_data
-        new_proj = Project.new(new_data)
-        raise('Could not save project') unless new_proj.save
+          JSON.pretty_generate(data: projects)
+        rescue StandardError
+          routing.halt 403, { message: 'Could not find any projects' }.to_json
+        end
 
-        response.status = 201
-        response['Location'] = "#{@proj_route}/#{new_proj.id}"
-        { message: 'Project saved', data: new_proj }.to_json
-      rescue Sequel::MassAssignmentRestriction
-        Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
-        routing.halt 400, { message: 'Illegal Attributes' }.to_json
-      rescue StandardError => e
-        Api.logger.error "PROJECT SAVING ERROR: #{e.message}"
-        routing.halt 500, { message: 'Unknown server error' }.to_json
+        # POST api/v1/projects
+        routing.post do
+          new_data = HttpRequest.new(routing).body_data
+          new_proj = @auth_account.add_owned_project(new_data)
+
+          response.status = 201
+          response['Location'] = "#{@proj_route}/#{new_proj.id}"
+          { message: 'Project saved', data: new_proj }.to_json
+        rescue Sequel::MassAssignmentRestriction
+          Api.logger.warn "MASS-ASSIGNMENT: #{new_data.keys}"
+          routing.halt 400, { message: 'Illegal Attributes' }.to_json
+        end
       end
     end
     # rubocop:enable Metrics/BlockLength